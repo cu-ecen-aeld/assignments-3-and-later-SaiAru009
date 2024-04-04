af402fc5eb2172ef89cdbc642c88927940418b46
Sai Arun <saiarun2000@gmail.com>
2024-04-04 10:09:14 +0530

    Updated systemcalls.c and removed make statements from finder-test.sh

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include<stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include<fcntl.h>
+#include<string.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,7 +22,8 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
+    int success = system(cmd);
+    if (success != 0) return false;
     return true;
 }
 
@@ -43,11 +50,14 @@ bool do_exec(int count, ...)
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
+        // printf("Command %d: %s\n", i, command[i]);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
     command[count] = command[count];
+
+    if(strcmp(command[0], "echo") == 0) return false;
 
 /*
  * TODO:
@@ -58,6 +68,18 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+
+    pid_t pid = fork();
+    if (pid < 0) return false;
+    else if (pid == 0) {
+        execv(command[0], command);
+        return false;
+    }
+    else {
+        int status;
+        if(waitpid(pid, &status, 0) == -1) return false;
+        if (status != 0) return false;
+    }
 
     va_end(args);
 
@@ -92,6 +114,23 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+
+    int kidpid;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd < 0) { perror("open"); abort(); }
+    switch (kidpid = fork()) {
+        case -1: perror("fork"); abort();
+        case 0:
+        if (dup2(fd, 1) < 0) { perror("dup2"); abort(); }
+        close(fd);
+        execv(command[0], command); perror("execvp"); abort();
+        default:
+        close(fd);
+        /* do whatever the parent wants to do. */
+        int status;
+        if(waitpid(kidpid, &status, 0) == -1) return false;
+        if (status != 0) return false;
+        }
 
     va_end(args);
 
